1fb86c7a93fb49e01cb20875b703d2edd5ac9faa
Noemie <noemiemsollier@gmail.com>
2021-04-16 19:56:29 -0500

    Third Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import "./Navbar.css";
 function Navbar() {
     return <nav className="navbar container">
         <h4><Link className="link" to='/rating'>Rating</Link></h4>
-        <h4><Link className="link" to='/upload'>Upload</Link></h4>
+        {/* <h4><Link className="link" to='/upload'>Upload</Link></h4> */}
     </nav>
 }
 
